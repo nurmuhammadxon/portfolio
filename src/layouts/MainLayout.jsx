f9fbf6a3d299ad46db4ccccfd5c0e7e4eda5122e
nurmuhammadxon <muhiddinovnurmuhammadxon1@gmail.com>
2025-05-21 23:54:41 +0500

    scroll top function add

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 // componetns
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 function MainLayout() {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    }, [location.pathname]);
+
     return (
         <div className='min-h-screen flex flex-col bg-lightMode text-softDark dark:bg-darkMode dark:text-gainsboro overflow-hidden'>
             <Header />
