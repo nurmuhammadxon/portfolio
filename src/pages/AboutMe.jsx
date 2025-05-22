de405a8676829bda2336b1c0cf40586d42aa8c88
nurmuhammadxon <muhiddinovnurmuhammadxon1@gmail.com>
2025-05-22 09:02:34 +0500

    resume link add

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaImage } from "react-icons/fa";
 import { mySkill } from '../util/constants'
+import { Link } from 'react-router-dom';
 
 function AboutMe() {
 
@@ -10,6 +11,17 @@ function AboutMe() {
                 <h2 className='text-lg sm:text-2xl font-semibold sm:font-bold mb-2 md:mb-4 animated animatedFadeInDown fadeInDown'>
                     Assalomu alaykum, men Muhiddinov — front-end dasturchiman.
                 </h2>
+                <div
+                    className='my-3.5 animated animatedFadeInUp fadeInUp'
+                    style={{ animationDelay: '300ms' }}
+                >
+                    <Link
+                        to='https://www.canva.com/design/DAGoG6KxCFo/hdlFEsCGodAYkPDCO5VNjA/edit?utm_content=DAGoG6KxCFo&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'
+                        className='text-blue-500 hover:text-blue-400 text-xl font-medium'
+                    >
+                        Resume
+                    </Link>
+                </div>
                 <p className='text-base sm:text-lg leading-relaxed text-justify text-gray-600 dark:text-[#BFBFBF] animated animatedFadeInUp fadeInUp'
                     style={{ animationDelay: '150ms' }}
                 >
@@ -28,12 +40,15 @@ function AboutMe() {
                     Maqsadim — foydalanuvchilar uchun qulay, ishonchli va zamonaviy veb-ilovalar yaratish hamda o‘z bilimlarimni doimiy ravishda oshirib borish.
                 </p>
             </div>
+            {/* my skills */}
             <div className="max-w-3xl w-full flex flex-col justify-center items-center">
-                <div className="w-full flex justify-center sm:justify-around flex-wrap gap-2.5 animated animatedFadeInUp fadeInUp">
+                <div className="w-full flex justify-center sm:justify-around flex-wrap gap-2.5 animated animatedFadeInDown fadeInDown"
+                    style={{ animationDelay: '350ms' }}
+                >
                     {mySkill.map((skil) => (
                         <div
                             key={skil.id}
-                            className="w-32 py-2.5 px-3.5 bg-gray-500 dark:bg-gray-800 flex flex-col items-center justify-center rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out"
+                            className="w-32 py-2.5 px-3.5 bg-gray-500 dark:bg-gray-800 flex flex-col items-center justify-center rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out hover:scale-105"
                         >
                             <div className="h-24 flex items-center justify-center">
                                 {skil.icon ? skil.icon : <FaImage className='text-8xl' />}
